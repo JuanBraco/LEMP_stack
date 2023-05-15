14e2238015a0575fe3706c6ae96de5a26ee38650
Juan Braco <107987620+ght2121@users.noreply.github.com>
2023-05-15 17:18:23 +0200

    first features

---
+++ wordpress/conf/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sleep 10
+
+wp config create --allow-root \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp core install  --allow-root \
+        --url=$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USR \
+        --admin_password=$WP_ADMIN_PWD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --skip-email
+
+wp user create $WP_USR $WP_EMAIL \
+        --role=author \
+        --user_pass=$WP_PWD \
+        --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+
+wp redis enable --allow-root
+
+/usr/sbin/php-fpm7.3 -F
