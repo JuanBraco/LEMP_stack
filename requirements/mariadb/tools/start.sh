b28b7dc429d2ba1c04e65719bbe0c1b14478d80f
Juan Braco <jbraco92@gmail.com>
2023-08-02 18:46:36 +0200

    fix

--- requirements/mariadb/tools/start.sh
+++ requirements/mariadb/tools/start.sh
@@ -2,12 +2,14 @@
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+echo "CREATE DATABASE IF NOT EXISTS '${SQL_DATABASE}';" > db.sql
+echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" >> db.sql
+echo "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}'.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql < db.sql
 
-exec mysqld_safe
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
