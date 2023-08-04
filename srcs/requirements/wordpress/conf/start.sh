741a806031805234d119bfbb390f57d09debedcf
Juan Braco <jbraco92@gmail.com>
2023-08-04 12:12:34 +0200

    cleaning

--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -17,11 +17,11 @@ wp core download --allow-root
 
 mv /var/www/wp-config.php /var/www/wordpress/
 
-#wp config create --allow-root \
-#        --dbname=$MYSQL_DATABASE \
-#        --dbuser=$MYSQL_USER \
-#        --dbpass=$MYSQL_PASSWORD \
-#        --dbhost=mariadb:3306
+wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb:3306
 
 wp core install  --allow-root \
         --url=$DOMAIN_NAME \
